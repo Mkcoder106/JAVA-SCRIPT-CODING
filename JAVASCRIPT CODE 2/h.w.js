bf67eb37cdba9d253216c3e27e354df84a76e0e5
Mkcoder106 <ghostlolman713@gmail.com>
2025-04-25 19:49:28 +0500

    fr

---
+++ JAVASCRIPT CODE 2/h.w.js
@@ -0,0 +1,16 @@
+var number1=18
+var number2=20
+var subtract =number1 - number2
+console.log(subtract)
+var number3=5
+var number4=3
+var add =number3 + number4
+console.log(add)
+var number5=7
+var number6=5
+var multiply=number5 * number6
+console.log(multiply)
+var number7=5
+var number8=20
+var division =number7 / number8
+console.log(division)
