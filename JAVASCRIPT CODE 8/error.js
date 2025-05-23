66a2946657dccbb4600c7b0f6c969ab9a6a2400f
Mkcoder106 <ghostlolman713@gmail.com>
2025-05-23 21:11:19 +0500

    dsadsads

---
+++ JAVASCRIPT CODE 8/error.js
@@ -0,0 +1,8 @@
+try {
+  addalert("Dear Visitor it seem's that you have made a mistake.Kindly fix that mistake");
+  console.log("pressure");
+
+}
+catch (err) {
+    console.log(err.message);
+}
---
+++ JAVASCRIPT CODE 8/string.js
@@ -0,0 +1,5 @@
+var str = "we are carnage";
+var search_position = str.search("carnage");
+var txt = str.replace("carnage","venom")
+console.log(search_position);
+console.log(txt);
