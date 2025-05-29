795c01df2d76032c8552af00527c734fe74f71dd
Mkcoder106 <ghostlolman713@gmail.com>
2025-05-29 20:16:59 +0500

    dsadad

---
+++ JAVASCRIPT CODE 9/json.js
@@ -0,0 +1,4 @@
+var myObj = {gorilla: "brr patapim",elephant: "liri lirlla",wood: "tung tung tung sahur"};
+var myJSON = JSON.stringify(myObj);
+var newObj = JSON.parse(myJSON);
+console.log(myObj.wood);
